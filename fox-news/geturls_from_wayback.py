573b90e1cb1912ebdfbf525324b1e05bcd4b2bfe
WeiKuoWei <ck3294@nyu.edu>
2024-03-29 00:03:58 +0400

    feat: minor changes

--- fox-news/geturls_from_wayback.py
+++ fox-news/geturls_from_wayback.py
@@ -7,27 +7,29 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import csv
 import time
+import pandas as pd
 
 
 
 # Define Chrome options
 chrome_options = Options()
 service = Service(executable_path="../chromedriver-mac-arm64/chromedriver")
-driver = webdriver.Chrome(service=service, options=chrome_options)
+
 
 def getURLS(file_path, export_csv_name):
     # import the website link from a CSV called urls.csv
     site_list = []
-    with open(file_path, 'r', encoding='utf-8') as csv_file:
+    with open("data/" + file_path, 'r', encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file)
         # next(csv_reader) # Skip the header if there is one
         for row in csv_reader:
-            site_list.append(row[0])
+            site_list.append(row[1])
 
     # Process the links
     processed_links = []        
     
-    for site in site_list:
+    for site in site_list[1:]:
+        driver = webdriver.Chrome(service=service, options=chrome_options)
         driver.get(site)
 
         # Wait for the page to load and find all articles
@@ -45,8 +47,20 @@ def getURLS(file_path, export_csv_name):
         # Close the driver after you're done
         driver.quit()
 
+        # pause the script for 5 seconds
+        time.sleep(5)
+
     # Append the URLs to a CSV file
     with open('data/' + export_csv_name, 'a', newline='', encoding='utf-8') as csv_file:
         csv_writer = csv.writer(csv_file)
         for link in processed_links:
             csv_writer.writerow([link])
+        
+        # drop the duplicates 
+        df = pd.read_csv('data/' + export_csv_name, header=None)
+        df.drop_duplicates(subset=0, inplace=True)
+
+        # drop the rows that contain the word 'video'
+        df = df[~df[0].str.contains('video')]
+
+        df.to_csv('data/' + export_csv_name, index=False, header=None)
