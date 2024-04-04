84aecd5d995723a91700faf728d371d40b2a8bc8
WeiKuoWei <ck3294@nyu.edu>
2024-04-04 13:50:04 +0400

    fix: minor updates on the main file

--- codes/main.py
+++ codes/main.py
@@ -1,74 +1,15 @@
 import time
+import json
 import sys
 
 from geturls import getURLS
-# from geturls_bbc import getURLS
-# from geturls_cnn import getURLS
-# from geturls_dailymail import getURLS
-# from geturls_foxnews import getURLS
-# from geturls_huffpost import getURLS
-# from geturls_nypost import getURLS
-# from geturls_nytimes import getURLS
-# from geturls_reuters import getURLS
-# from geturls_washingtontimes import getURLS
-
 from getarticles import articlestojson
 from wayback_machine import getArchiveURL
 
 
 START_YEAR = 2023
 END_YEAR = 2023
-
-site = {
-    0:{
-        "name":"bbc",
-        "url":"https://www.bbc.com/news/us-canada"
-    },
-    1:{
-        "name":"cnn",
-        "url":"https://edition.cnn.com/us"
-    },
-    2:{
-        "name":"dailymail",
-        "url":"https://www.dailymail.co.uk/ushome/index.html"
-    },
-    3:{
-        "name":"foxnews",
-        "url":"https://www.foxnews.com/category/us"
-    },
-    4:{
-        "name":"huffpost",
-        "url":"https://www.huffpost.com/news/us-news"
-    },
-    5:{
-        "name":"nypost",
-        "url":"https://nypost.com/us-news/"
-    },
-    6:{
-        "name":"nytimes",
-        "url":"https://www.nytimes.com/section/us"
-    },
-    7:{
-        "name":"reuters",
-        "url":"https://www.reuters.com/world/us/"
-    },
-    8:{
-        "name":"washingtonpost",
-        "url":"https://www.washingtonpost.com/national/"
-    },
-    9:{
-        "name":"washingtontimes",
-        "url":"https://www.washingtontimes.com/news/national/"
-    },
-    20:{
-        "name":"apnews",
-        "url":"https://apnews.com/us-news"
-    }
-
-}
-
-current_time = time.time()
-
+    
 def updateTime(function_name=""):
     global current_time
 
@@ -79,23 +20,28 @@ def updateTime(function_name=""):
 
 def main():
     print("Program started")
-    
+    # Load the data from the JSON file
+    with open('sites.json', 'r') as f:
+        sites = json.load(f)
+        
     try:
-        for i in range(1,2):
-            # get archive urls with wayback machine
-            getArchiveURL(site[i]['url'], START_YEAR, END_YEAR, "data/"+site[i]['name']+"/urls-wayback.csv")
-            updateTime("getArchiveURL")
-
-        # # get urls from news
-        # getURLS("urls-wayback.csv", "urls_uncleaned.csv", site[1]['name'])
-        # updateTime("getURLS")
+        # for i in range(5,6):
+        #     # get archive urls with wayback machine
+        #     getArchiveURL(sites[i]['url'], START_YEAR, END_YEAR, "data/"+sites[i]['name']+"/urls-wayback.csv")
+        #     updateTime("getArchiveURL")
+        
+        for i in range(8,10):
+            i = str(i)
+            getURLS("urls-wayback.csv", "urls_uncleaned.csv", sites[i]['name'])
+            updateTime("getURLS")
+        
 
         # # clean urls
-        # cleanURLS(site[1]+"urls_uncleaned.csv", site[1]+"urls_cleaned.csv")
+        # cleanURLS(sites[1]+"urls_uncleaned.csv", sites[1]+"urls_cleaned.csv")
         # updateTime("cleanURLS")
 
         # # get articles
-        # articlestojson(site[1]+"/urls_cleaned.csv")
+        # articlestojson(sites[1]+"/urls_cleaned.csv")
         # updateTime("articlestojson")
     
     except Exception as e:
@@ -121,8 +67,12 @@ apnews:
 - consider finding a substitute for this news website
 
 rerun nypost
+- only has urls from 2024/02-2024/03
+- consider finding a substitute for this news website
 
 bbc:
 - https://www.bbc.com/news/us-canada is the new website from 2024
 - https://www.bbc.com/news/world/us_and_canada is the one used from 2012-2023
+
+
 '''
