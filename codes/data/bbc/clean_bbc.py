ecd0fdb0f23352bd428ec3f4cdab36ace59a3d7c
WeiKuoWei <ck3294@nyu.edu>
2024-04-05 15:49:46 +0400

    feat: cleaned bbc csv file

---
+++ codes/data/bbc/clean_bbc.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# open urls_uncleaned.csv
+df = pd.read_csv('urls_uncleaned.csv', header=None, encoding='utf-8')
+
+# drop all the rows that do not contain 'us'
+df = df[df[0].str.contains('us-')]
+
+# drop all the rows that contains /live/
+df = df[~df[0].str.contains('/live/')]
+
+# add a new column to the dataframe
+df['status'] = 'no'
+
+# save the cleaned dataframe to urls_cleaned.csv
+df.to_csv('urls_cleaned.csv', header=None, index=False)
