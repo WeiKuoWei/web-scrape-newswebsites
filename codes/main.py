9ba32e7db12bdb59332d60cb6db8f95076b9555e
WeiKuo <ck3294@nyu.edu>
2024-04-06 18:16:01 +0400

    chore: processed 763 wayback urls for bbc

--- codes/main.py
+++ codes/main.py
@@ -2,8 +2,10 @@ import time
 import json
 import concurrent.futures
 import sys
+import pandas as pd
 
-from geturls import getURLS
+# from geturls import getURLS
+from geturls_soup import getURLS
 from getarticles import articlestojson
 from wayback_machine import getArchiveURL
 
@@ -20,9 +22,9 @@ def updateTime(function_name=""):
     print("Total time taken for {0} : {1}s".format(function_name, (end_time - current_time)))
     current_time = end_time
 
-def fetch_urls(site):
-    getURLS("urls-wayback.csv", "urls_uncleaned.csv", site['name'], site['base_url'])
-    updateTime("getURLS for " + site['name'])
+# def fetch_urls(site):
+#     getURLS("urls-wayback.csv", "urls_uncleaned.csv", site['name'], site['base_url'])
+#     updateTime("getURLS for " + site['name'])
 
 def main():
     print("Program started")
@@ -31,36 +33,44 @@ def main():
         sites = json.load(f)
         
     try:
-        for site in site_list:
-            url_num = len(sites[site]['url'])
-            for i in range(url_num):
-                i = str(i)
-                url_link = sites[site]['url'][i]["link"]
-                start_year = sites[site]['url'][i]["start_year"]
-                end_year = sites[site]['url'][i]["end_year"]
-                export_csv_path = "data/"+site+"/urls-wayback.csv"
+        # # wayback machine
+        # for site in site_list:
+        #     url_num = len(sites[site]['url'])
+        #     for i in range(url_num):
+        #         i = str(i)
+        #         url_link = sites[site]['url'][i]["link"]
+        #         start_year = sites[site]['url'][i]["start_year"]
+        #         end_year = sites[site]['url'][i]["end_year"]
+        #         export_csv_path = "data/"+site+"/urls-wayback.csv"
 
-                # get archive urls with wayback machine
-                getArchiveURL(url_link, start_year, end_year, export_csv_path)
+        #         # get archive urls with wayback machine
+        #         getArchiveURL(url_link, start_year, end_year, export_csv_path)
                 
-                # print("Getting archive urls for", url_link, "from", start_year, "to", end_year)
-                # print("Exporting to", export_csv_name)
+        #         updateTime("getArchiveURL")
 
-                updateTime("getArchiveURL")
+        # single thread
+        for site in site_list:
+            getURLS("urls-wayback.csv", "urls_uncleaned.csv", site, sites[site]['base_url'])
+            updateTime("getURLS for " + site)
 
-        # # single thread
-        # for i in range(0,10):
-        #     i = str(i)
-        #     getURLS("urls-wayback.csv", "urls_uncleaned.csv", sites[i]['name'], sites[i]['base_url'])
-        #     updateTime("getURLS")
-        
         # # multi-thread attempt
-        # # Extract the site data for the first 10 sites
-        # site_data = [sites[str(i)] for i in range(4)]
+        # single thread
+        # while True:
+        #     flag = False
+        #     for site in site_list:
+        #         df = pd.to_csv("data/"+site+"/urls-wayback.csv")
+        #         # if the third column contains entries that are not yes, set flag to True
+        #         if "no" in df[2].values or "fail" in df[2].values:
+        #             flag = True
+        #             break
+        #     if flag:
+        #         # use ProcessPoolExecutor to execute fetch_urls in parallel
+        #         with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+        #             executor.map(fetch_urls, site_list)
+        #         updateTime("getURLS")
 
-        # # Use ThreadPoolExecutor to execute fetch_urls in parallel
-        # with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        #     executor.map(fetch_urls, site_data)
+        #     else:
+        #         break
 
         # # get articles
         # for i in range(0,10):
