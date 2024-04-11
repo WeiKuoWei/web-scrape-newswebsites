f161a05ffacfc266be4f6bb631c93f6021ddfb1b
WeiKuo <ck3294@nyu.edu>
2024-04-11 11:35:29 +0400

    feat: update site list and get urls for daily beast

--- codes/main.py
+++ codes/main.py
@@ -14,7 +14,8 @@ from wayback_machine import getArchiveURL
 # START_YEAR = 2023
 # END_YEAR = 2023
 current_time = time.time()
-site_list = ["bbc", "cnn","foxnews","nationalreview","nytimes"]    
+# site_list = ["bbc", "cnn","foxnews","nationalreview","nytimes", "dailybeast", "washingtontimes", "newsweek"]    
+site_list = ["dailybeast", "washingtontimes", "newsweek"]    
 
 # Load the data from the JSON file
 with open('sites.json', 'r') as f:
@@ -49,20 +50,20 @@ def main():
     print("Program started")
         
     try:
-        # # wayback machine
-        # for site in site_list:
-        #     url_num = len(sites[site]['url'])
-        #     for i in range(url_num):
-        #         i = str(i)
-        #         url_link = sites[site]['url'][i]["link"]
-        #         start_year = sites[site]['url'][i]["start_year"]
-        #         end_year = sites[site]['url'][i]["end_year"]
-        #         export_csv_path = "data/"+site+"/urls-wayback.csv"
+        # wayback machine
+        for site in site_list:
+            url_num = len(sites[site]['url'])
+            for i in range(url_num):
+                i = str(i)
+                url_link = sites[site]['url'][i]["link"]
+                start_year = sites[site]['url'][i]["start_year"]
+                end_year = sites[site]['url'][i]["end_year"]
+                export_csv_path = "data/"+site+"/urls-wayback.csv"
 
-        #         # get archive urls with wayback machine
-        #         getArchiveURL(url_link, start_year, end_year, export_csv_path)
+                # get archive urls with wayback machine
+                getArchiveURL(url_link, start_year, end_year, export_csv_path)
                 
-        #         updateTime("getArchiveURL")
+                updateTime("getArchiveURL")
 
         # for site in site_list[2:3]:
         #     try:
@@ -70,34 +71,34 @@ def main():
         #     except Exception as e:
         #         print("Error: ", e, "; finishing using scraperapi credits")
 
-        # # multi-thread using ThreadPoolExecutor
-        # while True:
-        #     flag = False
-        #     try:
-        #         for site in site_list:
-        #             df = pd.read_csv("data/"+site+"/urls-wayback.csv", header=None, encoding='utf-8')
-        #             # if the third column contains entries that are not yes, set flag to True
-        #             if len(df.columns) >= 3 and ("no" in df[2].values or "fail" in df[2].values):
-        #                 flag = True
-        #                 break
+        # multi-thread using ThreadPoolExecutor
+        while True:
+            flag = False
+            try:
+                for site in site_list:
+                    df = pd.read_csv("data/"+site+"/urls-wayback.csv", header=None, encoding='utf-8')
+                    # if the third column contains entries that are not yes, set flag to True
+                    if len(df.columns) >= 3 and ("no" in df[2].values or "fail" in df[2].values):
+                        flag = True
+                        break
 
-        #     except Exception as e:
-        #         print("Error: ", e)
-        #         flag = True
+            except Exception as e:
+                print("Error: ", e)
+                flag = True
 
-        #     if flag:
-        #         # use ProcessPoolExecutor to execute fetch_urls in parallel
-        #         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        #             executor.map(fetch_urls, site_list)
-        #         updateTime("Finish getURLS")
+            if flag:
+                # use ProcessPoolExecutor to execute fetch_urls in parallel
+                with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+                    executor.map(fetch_urls, site_list)
+                updateTime("Finish getURLS")
 
-        #     else:
-        #         break
+            else:
+                break
 
-        # get articles
-        for site in site_list:
-            with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-                executor.map(fetch_articles, site)
+        # # get articles
+        # for site in site_list:
+        #     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        #         executor.map(fetch_articles, site)
     
     except Exception as e:
         print("Error: ", e)
